b5f2c6fb85826b834757689740e61c1a1fed41bc
Ben Visness <bvisness@gmail.com>
2020-01-26 12:39:30 -0600

    Improve local dev a bit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 
 import { App } from './components/App';
 
-const domContainer = document.querySelector('#react-root');
-ReactDOM.render(<App />, domContainer);
+if (window.NetworkTables) {
+	const domContainer = document.querySelector('#react-root');
+	ReactDOM.render(<App />, domContainer);
+} else {
+	const nope = document.querySelector('#nope')
+	nope.textContent = 'Failed to initialize NetworkTables. Check the console for more details.';
+	nope.classList.remove('hide');
+}
